dc89ac48cf93d0294f75886b56920e71593dd440
Manashi Thakur <manashiithakur808@gmail.com>
2026-06-25 15:34:23 +0545

    Initial Laravel project

--- database/migrations/2026_06_24_050928_create_exams_table.php
+++ database/migrations/2026_06_24_050928_create_exams_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('exam', function (Blueprint $table) {
+        Schema::create('exams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->date('date');
