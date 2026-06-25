1f925702b450741b996543ff2d5f53ca1646d00a
Manashi Thakur <manashiithakur808@gmail.com>
2026-06-25 16:23:12 +0545

    Updated users table migration

--- database/migrations/2026_06_24_095241_create_users_table.php
+++ database/migrations/2026_06_24_095241_create_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-           $table->id();
+            $table->id();
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('username');
